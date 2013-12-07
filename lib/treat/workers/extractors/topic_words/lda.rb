06f4dfaf7efc43a2f261fa52b9bc8b30af373bda
Tim White <timwhite47@gmail.com>
2013-12-06 20:46:55 -0800

    Revert "bugfix for stopwords"
    This reverts commit 31a7a8ba8213c34a0df43afe6d6f47293b539e01.

--- lib/treat/workers/extractors/topic_words/lda.rb
+++ lib/treat/workers/extractors/topic_words/lda.rb
@@ -42,16 +42,17 @@ class Treat::Workers::Extractors::TopicWords::LDA
   # Retrieve the topic words of a collection.
   def self.topic_words(collection, options = {})
     options = DefaultOptions.merge(options)
-    if list = options.delete(:stopwords)
+    if options.delete(:stopwords)
       stopwords = Tempfile.new('stopwords')
-      stopwords.puts list.to_yaml
+      stopwords.puts stopwords.to_yaml
       stopwords.flush
       options[:stopwords] = stopwords.path
     end
 
+
+
     docs = collection.documents.map { |d| d.to_s }
     # Create a corpus with the collection
-    binding.pry
     corpus = Lda::TextCorpus.new(docs, options[:stopwords])
     
     # Create an Lda object for training
